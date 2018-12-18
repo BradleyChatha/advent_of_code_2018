0d74b289f1d0420e55fdf5bdee3c4db5cf463ed8
Andy Radford/Aim-Educational <andy@aimeducational.org.uk>
2018-12-18 12:21:24 +0000

    Initial Commit

---
+++ day2/source/app.d
@@ -0,0 +1,289 @@
+import std.stdio;
+
+const INPUT = `lutrogkbetprmshdyfiqvzixaw
+lujnmgkbetprmshdyicqvzivaw
+lujnogkbetprmshdyfkqvtpxaw
+lujnqgkbetpmmshdywcqvzixaw
+lujnogtaetprmshdyffqvzixaw
+lujnogkbetprmuldyfcqvqixaw
+luunogkbetprlshdyfcqrzixaw
+lujnogkketprmshdyhcqvtixaw
+lyjnogzbetprmsbdyfcqvzixaw
+eujnogkbetprmshdyfrqvzixmw
+lujnogcbetprmshdyfvqvziqaw
+lujnogkbempruahdyfcqvzixaw
+lujlogkbetprmlhdyfyqvzixaw
+lrjnogkbetyrmshdofcqvzixaw
+lujnogkbetprmlheyfaqvzixaw
+lujoohkbetprmshdyfccvzixaw
+lijkogkietprmshdyfcqvzixaw
+lujnoukbetprmshdyfcqvzmxkw
+lujnogkbetprpshdyfcivzixiw
+lfjiogkbetprmshdyfwqvzixaw
+lujnogkbctprmshdffcqvzicaw
+lujnogkbetprashdckcqvzixaw
+lwjnogkbetprjshdyfcqvziyaw
+lujoogkbetpsmshdyfcnvzixaw
+lujnrgkjetprmsvdyfcqvzixaw
+luinogkcetprmshdyfccvzixaw
+lujnlgkbetprmshdyfcqvziirw
+rujnogkbetprmshdyfcqvzmxag
+luqnogkbetkrmshdhfcqvzixaw
+lujnogkbhxprmshdyfcbvzixaw
+lpjnogkbeyprmshdymcqvzixaw
+lujnogkbetdymshvyfcqvzixaw
+lujnoakbetprmshdlfcnvzixaw
+lujnogabetpumsydyfcovzixaw
+lpjnogkbetprmshdyfcqvzixzi
+gujnogkbevprdshdyfcqvzixaw
+lzjnogkbetprmsddyscqvzixaw
+lujnolkbmtprmshdyfuqvzixaw
+lujnogkbexpenshdyfcqvzixaw
+lujnogknetprmshdrfcqvzpxaw
+lujnogkbetpfmshayfcqvzwxaw
+lujnogkbetmrmshdyfcfvzilaw
+lujnogkqetpqdshdyfcqvzixaw
+xujnvgkbetprmnhdyfcqvzixaw
+lujnogkbetprsshdyfcqviixai
+rujnogkbeiprmshdyfcvvzixaw
+vujnogkeetprmshdyfcsvzixaw
+lujnogkbetcrmshdypcqxzixaw
+vuunogkbdtprmshdyfcqvzixaw
+nujnogkbetprmsvyyfcqvzixaw
+lujnogkbhtprmshdtfpqvzixaw
+lujnogkbetzrmphdyfcqvzixan
+lujnogkbetprmshdytcqvzixiu
+luanogkbeturmshdyfcqviixaw
+lujnogkbefprmshdyfcqvhixaf
+luvnogkbetprmsfdyfcqvzixhw
+lujnockbetprfshdyfcqvzixdw
+luenogkbetprmshdyfacvzixaw
+lfjnogkbetprmsldyfcxvzixaw
+xujnogkbetxrmshdufcqvzixaw
+lsjnogkbdtprmswdyfcqvzixaw
+lujnogkbetprishdyfcpvgixaw
+fujnogkbethrmshdyhcqvzixaw
+lujnogabetprmsydyfcovzixaw
+lvjnogkbetpvmshzyfcqvzixaw
+lujnogkbeturmshdufcqvcixaw
+lujnogkietprmshdyzyqvzixaw
+lujnogkaatprmshdyfwqvzixaw
+lujnogkbetpemshoyfcfvzixaw
+lujiogkbetpumshiyfcqvzixaw
+lujnvgkbetirmphdyfcqvzixaw
+lujnkgkbetprmhhdyfcqvzixmw
+lrjnorkbetprmphdyfcqvzixaw
+cujnogkietprmshdjfcqvzixaw
+lpjnogkbetpryshdyfcqvzixbw
+lujrogkbetprmchdzfcqvzixaw
+cujnlgkbetprmmhdyfcqvzixaw
+rujnogkbotprmshdyfcqczixaw
+lucnogknetprmshdyfcqvzixhw
+lujnogkbaturmdhdyfcqvzixaw
+lujnonkbetpcmshcyfcqvzixaw
+lujnogkbelprmshwyfcqvzihaw
+luanogkbetprmshdyhcqvzilaw
+lljnogkbetwrmshdyfcqxzixaw
+lujnogkbetprmshdyfcqejixaz
+lujnogkbetbrmshdyfcqizixgw
+lujeogkbetprkahdyfcqvzixaw
+lujnogkbevprmshssfcqvzixaw
+lutnogkbetgrmshdyfcqvmixaw
+lujnoppbntprmshdyfcqvzixaw
+lujnogkbetprmsmdyftqvdixaw
+lujuogubetprmshdyfcqvmixaw
+lujnorkbetprmsydyqcqvzixaw
+nujnogkbetprmshdyfcqvzifdw
+lujnogkbetpryshdygcqvziqaw
+ldjiogkbetprmuhdyfcqvzixaw
+lujnogkbetpsmghdyfcqvzixag
+xujnogkbetprmsrdefcqvzixaw
+lujnogkbejprmshiyfcqvzoxaw
+lujnogkletlnmshdyfcqvzixaw
+aujnogkbetprmwydyfcqvzixaw
+lujnogkbetprmsvdyfcrvzixap
+lujnogkbeeprmshdyfcqozixmw
+lujnogfbethrmshdyfcnvzixaw
+lujnogkbetprmshsrflqvzixaw
+lujuogkbnrprmshdyfcqvzixaw
+lutnogkbetpbmsgdyfcqvzixaw
+lujnqgkbetzrmshdyfcxvzixaw
+lujnogqbetprmshgxfcqvzixaw
+lujnogkbytprmshdyfcqvznxrw
+lujnogkbetprmshdyfcqvlidxw
+nljnogkbetprmsqdyfcqvzixaw
+lujnogkbetprmsidyfcqvfixjw
+lujnogkbetprmnhdyfcqvzifaq
+lujnogkbetprmwhdylcqvzixlw
+lujnogkbstprmshcyffqvzixaw
+lujnogkbetprmshdtfcqvzeyaw
+lujvzgkbetprmshdyncqvzixaw
+bujnogabetprmshdyfcqvzgxaw
+lujnogkbptpfmshdyfcrvzixaw
+ludnogkbetpsmshdyfcqvbixaw
+pgjnogkbttprmshdyfcqvzixaw
+lxjnogkbetprmshdyfcqezixyw
+lujnogkbutprmshdyfsqvzixjw
+lujnpgkbetlrmshdifcqvzixaw
+lujnogkbetprmshdygczvzixam
+lujnvgkwetirmshdyfcqvzixaw
+lujnogkbetprhshdyfcqvvixow
+lutnogkbetprmshdyfcrvzixgw
+lujnopkbetprmsudyfcqvzqxaw
+lujnolkbetprmjhdyftqvzixaw
+fujoogkbetprmshdyfcqvziuaw
+lqjnogkqetprmshdyfcqmzixaw
+lujnogkbetprmgcdyfcqvzibaw
+lujnogebebprmshdyzcqvzixaw
+lujnogkbetprmshzgfcnvzixaw
+luanogkretprmshdsfcqvzixaw
+nljnogkbetprmshdyfcqnzixaw
+lujnygkbetprmshdyfaqvzixjw
+lujnogkbetprmshdlfcqtzixag
+zujnogkbevprmshdyfcqvzixao
+lujnogkbbtprmshtyfcyvzixaw
+lujnogkbetjrmvhdyfcqvzixau
+fujnojkbetprmshdyfcqyzixaw
+suonogkbetprmshdyfcqvzexaw
+lujnogkbetprrshdyfcqvpifaw
+lujnogkbjtprmshdyfcqkzixat
+lujnogklxtprmshdyfcqyzixaw
+lujaogkbetprmshdyfcqvzihat
+kujnogkbetprmshlyfcqvjixaw
+lujnogkbetormshdyfcqvzbxap
+lujnogzuetpemshdyfcqvzixaw
+lujnogknetprmshdyfcwvlixaw
+lujnopkbetpfmshdyfcqxzixaw
+lqjnogkbetprmshdyfcoqzixaw
+lujnogkbetprmshdyfcdvzhdaw
+lujrogkbetprmshnyfcqvzijaw
+luovogkbetprpshdyfcqvzixaw
+xujnxgkbeturmshdyfcqvzixaw
+lujnogkbetprmshdyfyquzixav
+lujnogkbetptmshdyfcqvnitaw
+lujnoskbetprmvhdyfcqvziyaw
+dujnogkbetprmshdyfcqgzixae
+ltjnogkbetprmshdvfcqyzixaw
+luznogkbetprmshdyfcqvuixsw
+lujnogkbeiprmshdhfcqvzixav
+lujtogkbetormkhdyfcqvzixaw
+lujnogkbetprmssdyfcqvzilas
+lujnlgkbetprmshwyfcnvzixaw
+lujnogkbbtprmshdyfcqozfxaw
+lzjcogkpetprmshdyfcqvzixaw
+lujnogkketprmshdyfcqvzixfg
+oujnogkbetprgshdnfcqvzixaw
+zujnogkbetpbmshdyfcqvzixiw
+lcjnowkbetprmsmdyfcqvzixaw
+lujnogkbetprmsedyvcqvzzxaw
+lujnzikbetprmshdyfcqvzixlw
+lujnogkbetpyqsgdyfcqvzixaw
+lujnoukietprmssdyfcqvzixaw
+lujnogkbetyrmsydywcqvzixaw
+bujnogkdetprmsxdyfcqvzixaw
+lujnogkbetprmshdyjcqazixnw
+lgjnogktetprmsgdyfcqvzixaw
+lujnogkbeaprmahdyfcmvzixaw
+lubnoikbetprmshdyfcfvzixaw
+eusnogkbotprmshdyfcqvzixaw
+lujnogkbgtprmshdbfjqvzixaw
+lujnogkketprmshdyfcqvcjxaw
+lujnogkbetphmshdsfcqvzgxaw
+lujnhgkbdtprmshdyfeqvzixaw
+lujnogkbetpamsjdyfcqvlixaw
+lujnogkbetprmshlyfccvzxxaw
+lujzogkbetprmshdyfgqvzixpw
+lujnogkbetprfshdylcqveixaw
+lujnigkbetprmshdyfcqvzqxrw
+lujnogkbetpqmshdybcvvzixaw
+lujnogkbetprmsxdyfvqvzpxaw
+lujnogkbetprmfhdyfcqvmoxaw
+lujnoskbetprmhfdyfcqvzixaw
+lujnogkbetprmsodkfcqvzdxaw
+lujnogkbetpzrshdyfcqvzixpw
+mujnogkbetprmshdyfcsvzixan
+lujnonwbetprmshdyfcqvzixam
+lujnogkgetgrmshdyfcxvzixaw
+lujnogkbetpimshdyfcqvzixgs
+auanogkbetprmshdyfcxvzixaw
+lujnogkbenprishnyfcqvzixaw
+mujiogkbetpreshdyfcqvzixaw
+lujnogkbdipcmshdyfcqvzixaw
+kujnogkbetprmshdyucqvlixaw
+lujnogsbetprmshdrfcqvrixaw
+luynogkbetprhshdyfcqvsixaw
+lbjnogebetprmshdyfcqezixaw
+lujnogkbetprmnhtyfcqvzixap
+lujnogkbetprmszdhfcvvzixaw
+lujnogkbetprtshdywcqezixaw
+iuznogkbetprmshdyflqvzixaw
+nujnogkbetprmkhyyfcqvzixaw
+aujnogkbetprmshhyfcqvzijaw
+lujzogkbstprmshdyfcyvzixaw
+lujmogkbetprmshdtfctvzixaw
+ljjnogkbetrrmshdyfcqvzixaq
+lujnjgkbetprmshdyfdcvzixaw
+lujnogkbettrmshdyzcqvzfxaw
+dujnogybetvrmshdyfcqvzixaw
+lujnogkbetermshdmfcqvzvxaw
+lujnogkbetprmshdjfcdvzixab
+lujnogkbetirmshjyfcqvjixaw
+lujnogkbeiurmshdyfcqyzixaw
+lsjnogkbmtproshdyfcqvzixaw
+lujnogkqftprmshdyfcqvziwaw
+lujvogkbetprmshdyfcqvoixyw
+lujnogkoetprmshdyfpqvcixaw
+lujnogkbetprmshdyfcqvmghaw
+lujnmgkbetprmshdifcqveixaw
+lumlovkbetprmshdyfcqvzixaw
+lpjnogkretprmshdyfcqvfixaw
+luvnogpbetprmshdyftqvzixaw
+lujnogkbeturmshgymcqvzixaw
+lujiogkbbrprmshdyfcqvzixaw
+yujnogkbutprmshdyfcqvzixab
+lujvofubetprmshdyfcqvzixaw
+lujwogkbrtprmshdyfcqvnixaw
+lusnogkbetprmsydyfcqvzikaw
+lujnolkbewprmshdyfyqvzixaw
+lujnogkbetprvshdyfcivzikaw
+lujnogxbetprmszdyfzqvzixaw
+lujnogkvetprmxhdydcqvzixaw
+lujnogkbetpimsrdwfcqvzixaw
+lujnomboetprmshdyfcqvzixaw`;
+
+void main()
+{
+	import std.algorithm : splitter;
+	import std.array     : array;
+
+	auto lines = INPUT.splitter('\n').array;
+
+	foreach(i; 0..lines.length)
+	{
+		foreach(k; i+1..lines.length)
+		{
+			bool shouldSkip = false;
+			size_t index = size_t.max;
+			foreach(l; 0..lines[i].length)
+			{
+				if(lines[i][l] != lines[k][l])
+				{
+					if(index != size_t.max)
+					{
+						shouldSkip = true;
+						break;
+					}
+
+					index = l;
+				}
+			}
+			if(shouldSkip)
+				continue;
+
+			assert(index != size_t.max);
+			import std.algorithm : remove;
+
+			writeln(lines[i][0..index], lines[i][index+1..$]);
+		}
+	}
+}
